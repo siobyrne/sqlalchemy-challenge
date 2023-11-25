19db46b1b5a1a9c5e9a8072348f345405563cee6
siobyrne <siobyrne@users.noreply.github.com>
2023-11-25 18:07:01 -0500

    Add start date/end date routes

--- climate/app.py
+++ climate/app.py
@@ -4,12 +4,13 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 import datetime as dt
+import numpy as np
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///resources/hawaii.sqlite")
+engine = create_engine("sqlite:///climate/resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -37,6 +38,8 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# landing page
 @app.route("/")
 def homepage():
     return f"""
@@ -54,6 +57,7 @@ def homepage():
         </ul>
     """
 
+# precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     rows = session.query(m.date, m.prcp).filter(m.date >= prior_year).order_by(m.date).all()
@@ -61,6 +65,7 @@ def precipitation():
     session.close()
     return jsonify(precipitation)
 
+# stations
 @app.route("/api/v1.0/stations")
 def stations():
     rows = session.query(
@@ -83,7 +88,65 @@ def stations():
     session.close()
     return jsonify(results)
 
+# most active stations
+@app.route("/api/v1.0/tobs")
+def most_active():
+    rows = (
+        session.query(m.station, s.name, m.date, m.prcp, m.tobs)
+        .filter(m.station == "USC00519281")
+        .filter(m.date >= prior_year)
+        .join(s, m.station == s.station)
+        .all()
+    )
+    mas = [
+        {
+            "station": _.station,
+            "name": _.name,
+            "date": _.date,
+            "temp_obs": _.tobs,
+            "precipitation": _.prcp,
+        }
+        for _ in rows
+    ]
+    session.close()
+    return jsonify(mas)
 
+# pick a start date
+@app.route("/api/v1.0/<start_date>")
+def date(start_date):
+    data = session.query(func.min(m.tobs), func.max(m.tobs), func.avg(m.tobs)).\
+        filter(m.date >= start_date).all()
+
+    session.close()
+
+    data_list = list(np.ravel(data))
+
+    temp_min=data_list[0]
+    temp_max=data_list[1]
+    temp_avg=data_list[2]
+
+    output=(f"From date {start_date}, the minimum temperature is {temp_min}, the max temperature is {temp_max}, and the average temperature is {temp_avg}.")
+
+    return jsonify(output)
+
+# pick a start and end date
+@app.route("/api/v1.0//<start_date>/<end_date>")
+def dates(start_date, end_date=prior_year):
+    data = session.query(func.min(m.tobs), func.max(m.tobs), func.avg(m.tobs)).\
+        filter(m.date >= start_date).\
+        filter(m.date <= end_date).all()
+   
+    session.close()
+
+    data_list = list(np.ravel(data))
+
+    temp_min=data_list[0]
+    temp_max=data_list[1]
+    temp_avg=data_list[2]
+
+    output=(f"For the time period {start_date} to {end_date}, the minimum temperature is {temp_min}, the max temperature is {temp_max}, and the average temperature is {temp_avg}.")
+
+    return jsonify(output)
 
 # close session
 session.close()
